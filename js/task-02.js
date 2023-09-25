9b77f0dda6d4e412dc54d4ce0031b91940700750
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-09-25 16:38:26 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach((ingredient) => {
+  const container = document.querySelector("#ingredients");
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  container.append(li);
+});
