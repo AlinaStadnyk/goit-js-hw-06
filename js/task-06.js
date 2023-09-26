153ca31e6c68a17e27dd49917d1e8c3b28cbfaac
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-09-26 21:13:51 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,9 @@ input.addEventListener("blur", handlerValidation);
 function handlerValidation(event) {
   if (input.value.length === Number(input.dataset.length)) {
     input.classList.add("valid");
+    input.classList.remove("invalid");
   } else {
     input.classList.add("invalid");
+    input.classList.remove("valid");
   }
 }
