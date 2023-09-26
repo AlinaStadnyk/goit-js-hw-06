dba68baa5ee6f4aecc64b8996b1e964687da08a4
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-09-26 11:29:53 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  console.log(getRandomHexColor());
+}
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", handlerClick);
+function handlerClick(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
 }
