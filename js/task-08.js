609bb340ec1423752623c36827306b13af50546d
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-09-26 11:08:30 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(password.value.length);
+  if (email.value.length && password.value.length) {
+    console.log(data);
+  } else alert("Fill in all the fields");
+
+  event.currentTarget.reset();
+}
